a1ccee927469436374d508d28a436271001871e4
Adrien Gras <agr@owlnext.fr>
2024-02-19 15:35:12 +0100

    hive integrated

--- lib/skaara.dart
+++ lib/skaara.dart
@@ -1,7 +1,3 @@
 library skaara;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/entrypoint.dart' show Skaara;
--- test/skaara_test.dart
+++ test/skaara_test.dart
@@ -1,11 +1,24 @@
-import 'package:skaara/skaara.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:skaara/skaara.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+import 'includes/fake_path_provider_platform.dart';
+
+void main() async {
+  // This is required to initialize the PathProviderPlatform
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // This is required to override the default PathProviderPlatform for testing
+  PathProviderPlatform.instance = FakePathProviderPlatform();
+
+  // This is required to create a Skaara instance
+  Skaara skaara = await Skaara.create(enableLogging: true);
+
+  test('create skaara instance', () async {
+    expect(skaara, isNotNull);
+    expect(skaara.configuration.enableLogging, true);
+    expect(skaara.database, isNotNull);
+    expect(skaara.logger, isNotNull);
   });
 }
